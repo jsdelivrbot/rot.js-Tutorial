1dee439787457fec3472ed4d92a8e00d48712dd0
David Dever <david.dever@cox.net>
2015-01-24 21:32:56 -0800

    Key inputs and engine

--- game.js
+++ game.js
@@ -1,12 +1,20 @@
 var Game = {
     display: null,
     map: {},
+    engine: null,
+    player: null,
 
     init: function() {
         this.display = new ROT.Display();
         document.body.appendChild(this.display.getContainer());
 
         this._generateMap();
+
+        var scheduler = new ROT.Scheduler.Simple();
+        scheduler.add(this.player, true);
+
+        this.engine = new ROT.Engine(scheduler);
+        this.engine.start();
     },
 
     _generateMap: function() {
@@ -23,8 +31,17 @@ var Game = {
         digger.create(digCallback.bind(this));
 
         this._generateBoxes(freeCells);
-
         this._drawWholeMap();
+        this._createPlayer(freeCells);
+    },
+
+    _createPlayer: function(freeCells) {
+        var index = Math.floor(ROT.RNG.getUniform() * freeCells.length);
+        var key = freeCells.splice(index, 1)[0];
+        var parts = key.split(".");
+        var x = parseInt(parts[0]);
+        var y = parseInt(parts[1]);
+        this.player = new Player(x, y);
     },
 
     _generateBoxes: function(freeCells) {
@@ -43,5 +60,54 @@ var Game = {
             this.display.draw(x, y, this.map[key]);
         }
     }
+
 };
+
+var Player = function(x, y) {
+    this._x = x;
+    this._y = y;
+    this._draw();
+
+};
+
+Player.prototype.act = function() {
+    Game.engine.lock();
+    /* wait for user input; do stuff when user hits a key */
+    window.addEventListener("keydown", this);
+};
+
+Player.prototype.handleEvent = function(e) {
+    var keyMap = {};
+    keyMap[38] = 0;
+    keyMap[33] = 1;
+    keyMap[39] = 2;
+    keyMap[34] = 3;
+    keyMap[40] = 4;
+    keyMap[35] = 5;
+    keyMap[37] = 6;
+    keyMap[36] = 7;
+
+    var code = e.keycode;
+
+    if (!(code in keyMap)) { return; }
+
+    var diff = ROT.DIRS[8][keyMap[code]];
+    var newX = this._x + diff[0];
+    var newY = this._y + diff[1];
+
+    var newKey = newX + "," + newY;
+    if (!(newKey in Game.map)) { return; } /* cannot move in this direction */
+
+    Game.display.draw(this._x, this.y, Game.map[this._x+","+this.y]);
+    this._x = newX;
+    this._y = newY;
+    this._draw();
+    window.removeEventListener("keydown", this);
+    Game.engine.unlock();
+};
+
+Player.protoype._draw = function() {
+    Game.display.draw(this._x, this._y, "@", "#ff0");
+};
+
 
